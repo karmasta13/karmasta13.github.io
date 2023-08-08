d6d8315d649e84d552c9ad8bf2a8ec9e2b2e9ec0
karmasta13 <labbikarmacharya@gmail.com>
2023-08-08 19:28:01 +0545

    Add experience

---
+++ data.js
@@ -0,0 +1,97 @@
+const servicesData = [
+    {
+        title: "",
+        category: "ds",
+        experiences: [
+            {
+                experienceTitle: "Associate Data Scientist", // Add a unique title for each experience
+                date: "Aug 2022 - Current",
+                description: [
+                    "Created proof of concept real-time and non-real-time analytics to analyze large datasets and provide insights to inform business decisions.",
+                    "Developed identity fraud detection analysis using techniques such as anomaly detection, clustering, or classification, and reported findings to the business, leading to improved fraud detection and prevention measures",
+                    "Collaborated with cross-functional teams, including data engineers, business analysts, and product managers, to identify business problems, design analytics solutions, and communicate findings."
+                ],
+            }
+           
+        ],
+    },
+    {
+        title: "",
+        category: "da",
+        experiences: [
+            {
+                experienceTitle: "Data Visualization and Designer", // Add a unique title for each experience
+                date: "Nov 2021 - Aug 2022",
+                description: [
+                    "Conducted data analysis using Python and SQL to extract, transform, and report on product data as per client needs.",
+                    "Conducted interviews with clients to gather requirements and designed a prototype web-based user interface, incorporating user feedback and industry best practices using design tools such as Figma.",
+                    "Created a user-friendly and responsive UI for the admin website using front-end technologies."
+                    // ...
+                ],
+            },
+
+            {
+                experienceTitle: "Data Visualization and Design Intern ", // Add a unique title for each experience
+                date: "Aug 2021 - Nov 2021",
+                description: [
+                    "Designed and developed data visualizations for social media channels according to weekly sprints using Python, resulting in increased engagement and brand recognition",
+                    "Collaborated with the marketing team to create a data-driven marketing strategy to increase brand awareness and customer engagement.",
+                    // ...
+                ],
+            },
+        ],
+    },
+
+    {
+        title: "",
+        category: "design",
+        experiences: [
+            {
+                experienceTitle: "Graphic Designer Trainee", // Add a unique title for each experience
+                date: "Jul 2021 - Oct 2021",
+                description: [
+                    "Created visually appealing social media posts for the company and clients to increase engagement and brand recognition.",
+                    "Designed and developed a website for the company using WordPress, resulting in increased brand awareness",
+                    "Designed presentations for clients to communicate complex concepts and ideas in a visually compelling and easy-to-understand manner."
+                ],
+            },
+        ],
+    },
+
+    {
+        title: "",
+        category: "intern",
+        experiences: [
+            {
+                experienceTitle: "Program Extension Trainee", // Add a unique title for each experience
+                date: "Feb 2021 - Apr 2021",
+                description: [
+                    "Assisted Women LEAD Nepal in the implementation, planning, and designing of flagship program OWN IT Nepal and partnership program Women of the World Nepal with British Council.",
+                    "Coordinated with the event and program teams to ensure smooth execution of logistics, including venue arrangements, participant registration, and speaker management.",
+                    "Formulated comprehensive post-event reports that included evaluation of program success and recommended improvements for future events."
+                    // ...
+                ],
+            },
+            {
+                experienceTitle: "Fundraising Intern", // Add a unique title for each experience
+                date: "Nov 2019 - March 2020",
+                description: [
+                    "Coordinated end-of-year gifts and designed newsletters for overseas donors, resulting in increased engagement",
+                    "Wrote reports for Global Giving with supervisor, showcasing project impact and driving fundraising efforts.",
+                    "Assisted in the development and implementation of strategic sponsorship plans, managing ticket sales, and conducting outreach to cash, kind, and service sponsors, resulting in successful events and increased revenue."
+                    // ...
+                ],
+            },
+            {
+                experienceTitle: "Communication Intern", // Add a unique title for each experience
+                date: "Jul 2019 - Sep 2019",
+                description: [
+                    "Successfully implemented online recruitment strategies for the 2019 LEAD Course, reaching the target of 100 applications.Collaborated with co-intern to develop and execute effective outreach tactics.",
+                    "Formulated innovative outreach methods, including creating infographics, collecting alumni quotes, and producing alumni videos,to engage with potential applicants and promote the LEAD Course on social media.",
+                    "Designed visually appealing social media posts using Adobe Photoshop and Illustrator to further enhance online recruitment efforts, resulting in increased engagement and brand recognition."
+                    // ...
+                ],
+            },
+        ],
+    },
+];
